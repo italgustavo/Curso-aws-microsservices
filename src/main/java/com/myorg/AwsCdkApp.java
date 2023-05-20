d9e095bed33fb315a74ca4146f1032eb63ee13f4
Ítalo Gustavo <italgustavo@gmail.com>
2023-05-20 13:18:10 -0300

    Complete Service 01

--- src/main/java/com/myorg/AwsCdkApp.java
+++ src/main/java/com/myorg/AwsCdkApp.java
@@ -1,18 +1,23 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
-import software.amazon.awscdk.StackProps;
-
-import java.util.Arrays;
 
 public class AwsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new VpcStack(app, "Vpc");
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
+
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
+
+        RdsStack rdsStack = new RdsStack(app, "Rds", vpcStack.getVpc());
+        rdsStack.addDependency(vpcStack);
+
+        ServiceStack service01Stack = new ServiceStack(app, "Service01", clusterStack.getCluster());
+        service01Stack.addDependency(clusterStack);
+        service01Stack.addDependency(rdsStack);
 
         app.synth();
     }
 }
-
